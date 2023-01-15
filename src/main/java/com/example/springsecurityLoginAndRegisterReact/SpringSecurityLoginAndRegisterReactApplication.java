6db8e1f60d72ad5412db5764f72b1a2856cd39d7
Jiwoon <kfromh0136@Naver.com>
2023-01-15 15:15:44 +0900

    #1-스프링부트 프로젝트 생성
    `Spring Boot DevTools`
    `Lombok`
    `spring web`
    `Validation`
    `Spring Data JPA `
    `MySQL Driver`

---
+++ src/main/java/com/example/springsecurityLoginAndRegisterReact/SpringSecurityLoginAndRegisterReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityLoginAndRegisterReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLoginAndRegisterReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLoginAndRegisterReactApplication.class, args);
+	}
+
+}
